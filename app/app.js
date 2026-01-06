e2577c157d463c2d440739ddf8eb7a1d9ad59d73
dimasprastyoaditama <prastyod647@gmail.com>
2026-01-06 15:52:32 +0700

    setup Docker

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
-// TODO: Ini adalah titik masuk aplikasi, setup Express, Middleware, dan Server Listener disini
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Halo! Server GearHike Berhasil Jalan!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
